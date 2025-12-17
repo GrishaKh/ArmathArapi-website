1bbb371cd3a3a706a867c005a2e712e5c8bf6570
Grisha <grisha.khachatrian@gmail.com>
2025-12-18 02:15:21 +0400

    fix: remove invalid characters from lib/translations.ts causing build error

--- lib/translations.ts
+++ lib/translations.ts
@@ -364,4 +364,3 @@ export const translations = {
 
 export type Language = keyof typeof translations
 export type TranslationKey = keyof typeof translations.en
-                                                                                                                                                                                       
